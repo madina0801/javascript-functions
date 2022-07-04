ec594ac82085212d006849e60e85e23f9920fa51
Madina <madinatussupova0820@gmail.com>
2022-07-04 15:07:54 +0600

    Call and Apply methods

--- script.js
+++ script.js
@@ -61,6 +61,8 @@ checkIn(flight, jake);
 
 // FUNCTIONS ACCEPTING CALLBACK FUNCTIONS
 
+/*
+
 const oneWord = function(str) {
 	return str.replaceAll(' ', '').toLowerCase();
 }
@@ -78,6 +80,8 @@ const transformer = function(str, fn) {
 	console.log(`Transformed by: ${fn.name}`);
 }
 
+// JS uses callbacks all the time
+
 transformer('JavaScript is the best!', upperFirstWord);
 transformer('JavaScript is the best!', oneWord);
 
@@ -85,4 +89,71 @@ const high5 = function() {
 	console.log('👋🏻');
 }
 
-document.body.addEventListener('click', high5)
+document.body.addEventListener('click', high5);
+['Jonas', 'Jake', 'Adam'].forEach(high5);
+*/
+
+// FUNCTIONS RETURNING FUNCTIONS
+
+const greet = function(greeting) {
+	return function(name) {
+		console.log(`${greeting} ${name}!`);
+	}
+}
+
+const greeterHey = greet('Hey');
+greeterHey('Jonas');
+greeterHey('Steven');
+
+greet('Hello')('Jake');
+
+const greetArrow = greeting => name => console.log(`${greeting} ${name}!`);
+
+greetArrow('Goodnight')('Jay');
+
+// 
+
+const lufthansa = {
+	airline: 'Lufthansa',
+	iataCode: 'LH',
+	bookings: [],
+	book(flightNum, name) {
+		console.log(`${name} booked a set on ${this.airline} flight ${this.iataCode}${flightNum}`);
+		this.bookings.push({flight: `${this.iataCode}${flightNum}`, name});
+	}
+}
+
+const book = lufthansa.book;
+
+lufthansa.book(123, 'Jake Lee');
+lufthansa.book(789, 'John Smith');
+console.log(lufthansa);
+
+const eurowings = {
+	airline: 'Eurowings',
+	iataCode: 'EW',
+	bookings: []
+};
+
+// Call method
+
+book.call(eurowings, 23, 'Adam Williams');
+console.log(eurowings);
+
+book.call(lufthansa, 239, 'James Cooper');
+
+const swiss = {
+	airline: 'Swiss Air Lines',
+	iataCode: 'LX',
+	bookings: []
+}
+
+book.call(swiss, 575, 'Niki Jones');
+console.log(swiss);
+
+// Apply method
+const flightData = [583, 'George Cooper'];
+book.apply(swiss, flightData);
+console.log(swiss);
+
+book.call(swiss, ...flightData);
