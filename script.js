6ae3c1a2c171fb550602cf17216eb5552db5c187
Madina <madinatussupova0820@gmail.com>
2022-07-12 12:28:34 +0600

    Closures with examples

--- script.js
+++ script.js
@@ -254,3 +254,70 @@ runOnce();
 })();
 
 (() => console.log('This will never run again'))();
+
+// Closures
+
+const secureBooking = function() {
+	let passengerCount = 0;
+
+	return function() {
+		passengerCount++;
+		console.log(`${passengerCount} passengers`);
+	}
+}
+
+const booker = secureBooking();
+booker();
+booker();
+booker();
+
+// Closures Example 1
+
+let f;
+
+const g = function() {
+	const a = 23;
+	f = function() {
+		console.log(a * 2);
+	}
+};
+
+const h = function() {
+	const b = 777;
+	f = function() {
+		console.log(b * 2);
+	}
+}
+
+g();
+f();
+console.dir(f);
+// Reassigning f function
+h();
+f();
+console.dir(f);
+
+// Closures Example 2
+const boardPassengers = function(n, wait) {
+	const perGroup = n / 3;
+
+	setTimeout(function(){
+		console.log(`We are now boarding all ${n} passengers`);
+		console.log(`There are 3 groups, each with ${perGroup} passengers`);
+	}, wait * 1000);
+
+	console.log(`Will start boarding in ${wait} seconds`);
+}
+
+boardPassengers(180, 3);
+
+// CHALLENGE 2
+
+(function() {
+	const header = document.querySelector('h1');
+	header.style.color = 'red';
+
+	document.querySelector('body').addEventListener('click', function() {
+			header.style.color = 'blue';
+	})
+})();
